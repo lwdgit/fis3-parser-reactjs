7b2aa4da72d9740cc49b0c8d272c48295bc4fb8c
lwdos <1127622940@qq.com>
2015-10-19 20:12:25 +0800

    fix error pause

--- index.js
+++ index.js
@@ -2,17 +2,21 @@
 
 var react = require('jstransform/simple');
 
-module.exports = function(content,file){
+module.exports = function(content,file, conf){
+    conf = conf || {};
+    conf.react = true;
+    conf.es6 = true;
+    conf.es7 = true;
+    conf.harmony = true;
+    conf.target = conf.target || 'es3';//for ie8
+    conf.sourceMap = conf.sourceMap === true ? true : false;
+    conf.sourceMapInline = conf.sourceMapInline === false ? false : conf.sourceMap ? true : false;
+    
     try {
-        return react.transform(content, {
-           react: true,
-           es6: true,
-           es7: true,
-           harmony: true,
-           target: 'es3'//for ie8
-        }).code;
+        return react.transform(content, conf).code;
     } catch(e) {
-        fis.log.error(file.toString(), e);
+        fis.log.warn(file.toString(), e);
+        process.stdout.write('\x07');
         return '';
     }
 };
